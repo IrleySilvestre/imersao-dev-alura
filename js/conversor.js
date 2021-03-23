c182d72e7125da96bfa243ed09055c34573b7cc8
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-03-23 00:47:03 -0300

    Layout inicial e pagina exercicio 1 quese finalizada

---
+++ js/conversor.js
@@ -0,0 +1,36 @@
+const formConversao = document.querySelector('[data-form="form-conversao"]')
+
+
+formConversao.addEventListener('submit', (evento) =>{
+    evento.preventDefault()
+    const valorEmReal = evento.target.querySelector("[data-tipo=valor]").value
+    converteMoedas(valorEmReal)
+})
+
+function converteMoedas(valor){
+    const url = fetch( 'http://economia.awesomeapi.com.br/json/all')
+        .then( moedas => moedas.json())
+        .then( moedas => {
+              pesquisaValores(moedas, valor)
+        })
+}
+
+function pesquisaValores(moedas, valor){
+    const vlrConvertido = document.querySelector('[data-tipo="valorConvertido"]')
+    const dataCotacao = document.querySelector('[data-tipo="dataCotacao"]')
+    const siglaMoeda = (Object.values(moedas))
+
+    Object.values(siglaMoeda).forEach((item)=>{
+        // console.log(`${item.name}-${item.code} Compra: ${item.bid} Venda: ${item.ask} Variacao Maxima:${item.high} Minima:${item.low}`)
+        if (item.code === 'USD'){
+            dataCotacao.textContent = item.create_date
+            dataCotacao.innerHTML
+
+            vlrConvertido.textContent = valor * item.bid
+            vlrConvertido.innerHTML
+
+
+        }
+    })
+}
+
