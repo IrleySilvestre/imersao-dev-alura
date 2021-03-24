34a1b2bd24bf994b37f775e623d6d6cd85ee18f2
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-03-23 22:53:20 -0300

    Final Aula 1

--- js/conversor.js
+++ js/conversor.js
@@ -1,10 +1,27 @@
+const input = document.querySelector('[data-tipo="valor"]')
+input.addEventListener('blur', (evento) =>{
+    SimpleMaskMoney.setMask(input, {
+    prefix: 'R$ ',
+    fixed: true,
+    fractionDigits: 2,
+    decimalSeparator: ',',
+    thousandsSeparator: '.',
+    cursor: 'end'
+
+    })
+})
+
 const formConversao = document.querySelector('[data-form="form-conversao"]')
 
 
 formConversao.addEventListener('submit', (evento) =>{
     evento.preventDefault()
     const valorEmReal = evento.target.querySelector("[data-tipo=valor]").value
-    converteMoedas(valorEmReal)
+
+    const somenteNumero = (valorEmReal.replace('R$',""))
+    const semPonto = somenteNumero.replace('.',"")
+    const numero = parseFloat(semPonto.replace(',', "."))
+    converteMoedas(numero)
 })
 
 function converteMoedas(valor){
@@ -16,21 +33,45 @@ function converteMoedas(valor){
 }
 
 function pesquisaValores(moedas, valor){
-    const vlrConvertido = document.querySelector('[data-tipo="valorConvertido"]')
     const dataCotacao = document.querySelector('[data-tipo="dataCotacao"]')
+    const tabelaMoedas = document.querySelector('[data-tipo="container-cotacao"]')
+    const conteudoTabelaMoedas = document.createElement('dl')
+    const listTabela = []
+
     const siglaMoeda = (Object.values(moedas))
 
     Object.values(siglaMoeda).forEach((item)=>{
         // console.log(`${item.name}-${item.code} Compra: ${item.bid} Venda: ${item.ask} Variacao Maxima:${item.high} Minima:${item.low}`)
-        if (item.code === 'USD'){
-            dataCotacao.textContent = item.create_date
+
+            dataCotacao.textContent = `Horario da Contação:  ${moment(item.create_date).format('DD/MM/YYYY HH:mm:ss')}`
             dataCotacao.innerHTML
+            listTabela.push(
+                `
+                    <dt class="titulo-cotacao">${item.name} - ${item.code} <span class="valor-convertido">${(valor * item.bid).toFixed(2)}</span> </dt>
+                    <dt>
+                         <table class="table-valores" >
+                            <tbody>
+                                <tr class="cabecalho-tabela">
+                                    <th class="linha-tabela">Venda</th>
+                                    <th class="linha-tabela">Compra</th>
+                                   <th class="linha-tabela">Maximo - Dia</th>
+                                    <th class="linha-tabela">Minimo - Dia</th>
+                                </tr>
+                                <tr class="cabecalho-tabela">
+                                    <td class="linha-tabela">${parseFloat(item.ask).toFixed(2)}</td>
+                                    <td class="linha-tabela">${parseFloat(item.bid).toFixed(2)}</td>
+                                    <td class="linha-tabela">${parseFloat(item.high).toFixed(2)}</td>
+                                    <td class="linha-tabela">${parseFloat(item.low).toFixed(2)}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </dt>
+                `
+            )
 
-            vlrConvertido.textContent = valor * item.bid
-            vlrConvertido.innerHTML
-
-
-        }
     })
+    conteudoTabelaMoedas.innerHTML=listTabela
+    tabelaMoedas.appendChild(conteudoTabelaMoedas)
+
 }
 
